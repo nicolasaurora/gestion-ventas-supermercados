9bf7f05bcb143aba890277553e4e6a2eb604e84d
nicolasaurora <nicolas.aurora5@gmail.com>
2026-03-10 11:37:24 -0300

    Seguimos armando la capa de servicio.

--- GestionVentasSupermercado/src/main/java/com/supermercado/GestionVentasSupermercado/Service/VentaService.java
+++ GestionVentasSupermercado/src/main/java/com/supermercado/GestionVentasSupermercado/Service/VentaService.java
@@ -2,18 +2,36 @@ package com.supermercado.GestionVentasSupermercado.Service;
 
 import com.supermercado.GestionVentasSupermercado.Dto.SucursalDTO;
 import com.supermercado.GestionVentasSupermercado.Dto.VentaDTO;
+import com.supermercado.GestionVentasSupermercado.Model.Venta;
+import com.supermercado.GestionVentasSupermercado.Repository.VentaRepository;
+import org.modelmapper.ModelMapper;
 
 import java.util.List;
 
 public class VentaService implements IVentaService {
+    private final VentaRepository ventaRepository;
+    private final ModelMapper modelMapper;
+
+    public VentaService(VentaRepository ventaRepository, ModelMapper modelMapper) {
+        this.ventaRepository = ventaRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public VentaDTO create(VentaDTO ventaDto) {
-        return null;
+        Venta venta = modelMapper.map(ventaDto, Venta.class);
+
+        Venta ventaNueva = ventaRepository.save(venta);
+
+        return modelMapper.map(ventaNueva, VentaDTO.class);
     }
 
     @Override
     public VentaDTO getVentaById(Long id) {
-        return null;
+        Venta ventaEncontrada = ventaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("La venta con ID: " + id + " no existe."));
+
+        return modelMapper.map(ventaEncontrada, VentaDTO.class);
     }
 
     @Override
